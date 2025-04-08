95ccc28b31f8173e882b7755fc4c88609d954e84
hey-sakshi <128986605+hey-sakshi@users.noreply.github.com>
2025-04-08 20:23:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -678,4 +678,5 @@ def generate_study_plan():
     return redirect(url_for('view_study_plan'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
